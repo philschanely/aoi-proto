1d5fdccbee784941bada81e8dddad9616bd6bb88
Phil Schanely <philschanely@cedarville.edu>
2017-01-02 16:06:00 -0500

    Updates to source organization and Handlebars compiling helpers.

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,13 @@ $(function(){
     });
 
     $.getJSON("data/categories.json", function(data){
-        var nav = Handlebars.compile($("nav").html());
+        var nav = Handlebars.compile($("#nav").html());
         var navHTML = nav(data);
         $("nav").html(navHTML);
     });
 
     $.getJSON("data/tasks.json", function(data){
-        var tasks = Handlebars.compile($("main").html());
+        var tasks = Handlebars.compile($("#tasks").html());
         var tasksHTML = tasks(data);
         $("main").html(tasksHTML);
     });
