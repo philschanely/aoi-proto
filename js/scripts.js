4d2e21fb486cdf5b87b9eaff7946020cac397e4e
Philip J Schanely <philschanely@cedarville.edu>
2017-01-04 13:35:08 -0500

    All final styles and markup

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,31 @@ $(function(){
     $.getJSON("data/categories.json", function(data){
         var nav = Handlebars.compile($("#nav").html());
         var navHTML = nav(data);
-        $("nav").html(navHTML);
+        $("#app-nav").html(navHTML);
     });
 
     $.getJSON("data/tasks.json", function(data){
         var tasks = Handlebars.compile($("#tasks").html());
         var tasksHTML = tasks(data);
-        $("main").html(tasksHTML);
+        $("#task-lists").html(tasksHTML);
+    });
+
+    $.getJSON("data/task.json", function(data){
+        var taskForm = Handlebars.compile($("#edit-task").html());
+        var taskFormHTML = taskForm(data);
+        $("#modal .modal-content").html(taskFormHTML);
+    });
+
+    $("body").on("click", function(e){
+        var $target = $(e.target);
+        if ($target.closest(".task").length > 0) {
+            e.preventDefault();
+            $("#modal").addClass("on");
+        }
+        if ($target.hasClass("modal")) {
+            e.preventDefault();
+            $("#modal").removeClass("on");
+        }
     });
 
 });
